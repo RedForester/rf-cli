626ed82efc29c5a554600b2083179e9548f525e8
deissh <deissh@yandex.ru>
2021-07-19 17:29:16 +0300

    feat(ext/init): prompt ext info

--- pkg/command/extension/init/init.go
+++ pkg/command/extension/init/init.go
@@ -3,16 +3,37 @@ package init
 import (
 	"fmt"
 	"github.com/AlecAivazis/survey/v2"
+	"github.com/deissh/rf-cli/internal/config"
+	"github.com/deissh/rf-cli/pkg/extension"
 	"github.com/deissh/rf-cli/pkg/factory"
 	"github.com/deissh/rf-cli/pkg/utils"
 	"github.com/spf13/cobra"
+	"os"
 )
 
+type Options struct {
+	Interactive bool
+}
+
 func NewCmdExtInit(f *factory.Factory) *cobra.Command {
+	opt := Options{
+		Interactive: true,
+	}
+
 	cmd := &cobra.Command{
 		Use:   "init",
 		Short: "Create new manifest",
-		Run:   run,
+		Run: func(cmd *cobra.Command, args []string) {
+			cfg, err := f.Config()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			if opt.Interactive {
+				interactiveExtInfo(cfg)
+			}
+		},
 	}
 
 	utils.DisableAuthCheck(cmd)
@@ -20,23 +41,50 @@ func NewCmdExtInit(f *factory.Factory) *cobra.Command {
 	return cmd
 }
 
-func run(cmd *cobra.Command, args []string) {
+func interactiveExtInfo(cfg *config.Config) *extension.Extension {
+	extInfo := &extension.Extension{
+		ID:    "NOT_CREATED",
+		Email: cfg.Client.Username,
+	}
 
-}
-
-func interactiveExtInfo() {
 	q := []*survey.Question{
 		{
-			Name: "chooseGitIgnore",
-			Prompt: &survey.Select{
-				Message: "Choose a .gitignore template",
+			Name: "name",
+			Prompt: &survey.Input{
+				Message: "Extensions name:",
 			},
+			Validate:  survey.Required,
+			Transform: survey.Title,
+		},
+		{
+			Name: "Description",
+			Prompt: &survey.Input{
+				Message: "Description:",
+			},
+		},
+		{
+			Name: "email",
+			Prompt: &survey.Input{
+				Message: "Author email:",
+				Default: extInfo.Email,
+			},
+			Validate: survey.Required,
+		},
+		{
+			Name: "baseURL",
+			Prompt: &survey.Input{
+				Message: "Extension base url:",
+				Help:    "for example: https://localhost:2300",
+			},
+			Validate: survey.Required,
 		},
 	}
 
-	err := survey.Ask(q, nil)
+	err := survey.Ask(q, extInfo)
 	if err != nil {
-		fmt.Errorf("err: %e", err)
-		return
+		fmt.Println(err)
+		return nil
 	}
+
+	return extInfo
 }
