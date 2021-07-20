ff60bb753163c8e5c12aed059da6b6f27a225105
deissh <deissh@yandex.ru>
2021-07-20 16:29:36 +0300

    feat(ext/list): output formats

--- pkg/command/extension/list/list.go
+++ pkg/command/extension/list/list.go
@@ -1,6 +1,7 @@
 package list
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/deissh/rf-cli/internal/config"
 	"github.com/deissh/rf-cli/pkg/extension"
@@ -15,23 +16,28 @@ type Options struct {
 	HttpClient func() (*http.Client, error)
 	Config     func() (config.Config, error)
 
-	Limit int
-	Owner string
+	Limit  int
+	Owner  string
+	Format string
 }
 
 func NewCmdExtList(f *factory.Factory) *cobra.Command {
+	var opt Options
+
 	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "Return all registered extensions",
 		Run: func(cmd *cobra.Command, args []string) {
-			run(f, cmd, args)
+			run(f, cmd, args, opt)
 		},
 	}
+
+	cmd.Flags().StringVar(&opt.Format, "format", "pretty", "output format (json, pretty-json, pretty)")
 
 	return cmd
 }
 
-func run(f *factory.Factory, cmd *cobra.Command, args []string) {
+func run(f *factory.Factory, cmd *cobra.Command, args []string, opt Options) {
 	cfg, err := f.Config()
 	if err != nil {
 		fmt.Printf("internal error, err: %s \n", err)
@@ -44,8 +50,8 @@ func run(f *factory.Factory, cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
-	opt := rf_api.NewOptions(cfg.Rf.BaseURL)
-	api := rf_api.New(client, opt)
+	apiOpts := rf_api.NewOptions(cfg.Rf.BaseURL)
+	api := rf_api.New(client, apiOpts)
 
 	data, err := api.Ext.GetAll()
 	if err != nil {
@@ -53,10 +59,22 @@ func run(f *factory.Factory, cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
-	printExtensions(data)
+	switch opt.Format {
+	case "json":
+		str, _ := json.Marshal(data)
+		fmt.Println(string(str))
+	case "pretty-json":
+		str, _ := json.MarshalIndent(data, "", " ")
+		fmt.Println(string(str))
+	case "pretty":
+		prettyPrint(data)
+	default:
+		fmt.Println("format not found")
+		os.Exit(1)
+	}
 }
 
-func printExtensions(data *[]extension.Extension) {
+func prettyPrint(data *[]extension.Extension) {
 	fmt.Println("ID                                     NAME")
 	for _, ext := range *data {
 		fmt.Printf("%s   %s\n", ext.ID, ext.Name)
