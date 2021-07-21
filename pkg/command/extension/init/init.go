e30c1eaaaa9400ed6b1805e6abd16e1426d3dc67
deissh <deissh@yandex.ru>
2021-07-21 20:30:58 +0300

    feat(ext/init): простой manifest.yaml

--- pkg/command/extension/init/init.go
+++ pkg/command/extension/init/init.go
@@ -8,11 +8,13 @@ import (
 	"github.com/deissh/rf-cli/pkg/factory"
 	"github.com/deissh/rf-cli/pkg/utils"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 	"os"
 )
 
 type Options struct {
 	Interactive bool
+	File        string
 }
 
 func NewCmdExtInit(f *factory.Factory) *cobra.Command {
@@ -24,21 +26,45 @@ func NewCmdExtInit(f *factory.Factory) *cobra.Command {
 		Use:   "init",
 		Short: "Create new manifest",
 		Run: func(cmd *cobra.Command, args []string) {
-			cfg, err := f.Config()
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-
-			if opt.Interactive {
-				interactiveExtInfo(cfg)
-			}
+			run(f, cmd, args, opt)
 		},
 	}
+
+	cmd.Flags().StringVarP(&opt.File, "file", "f", "manifest.yaml", "manifest file path")
 
 	utils.DisableAuthCheck(cmd)
 
 	return cmd
+}
+
+func run(f *factory.Factory, cmd *cobra.Command, args []string, opt Options) {
+	cfg, err := f.Config()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var ext *extension.Extension
+	if opt.Interactive {
+		ext = interactiveExtInfo(cfg)
+	} else {
+		// todo
+	}
+
+	data, err := yaml.Marshal(ext)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	file, err := os.OpenFile(opt.File, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+	file.Write(data)
 }
 
 func interactiveExtInfo(cfg *config.Config) *extension.Extension {
